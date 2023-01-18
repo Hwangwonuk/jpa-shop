df3dbeabb3790abcf40b12ad66d61f1f6ec3d81f
Hwangwonuk <hok0173@naver.com>
2023-01-18 10:31:20 +0900

    # 웹 계층 개발 1. 홈 화면과 레이아웃 2. 회원 등록 3. 회원 목록 조회 4. 상품 등록 5. 상품 목록 6. 상품 수정 7. 변경 감지와 병함 8. 상품 주문 9. 주문 목록 검색, 취소

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,36 @@
+/*
+ * Created by Wonuk Hwang on 2023/01/17
+ * As part of Bigin
+ *
+ * Copyright (C) Bigin (https://bigin.io/main) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by infra Team <wonuk_hwang@bigin.io>, 2023/01/17
+ */
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * create on 2023/01/17. create by IntelliJ IDEA.
+ *
+ * <p> </p>
+ * <p> {@link } and {@link } </p> *
+ *
+ * @author wonukHwang
+ * @version 1.0
+ * @see
+ * @since (ex : 5 + 5)
+ */
+@Controller
+@Slf4j
+public class HomeController {
+
+  @RequestMapping("/")
+  public String home() {
+    log.info("home controller");
+    return "home";
+  }
+}
---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        /**
+         * BindingResult - 오류가 발생하면 해당 객체에 담겨지고 아래의 코드가 실행된다.
+         */
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    /**
+     * 외부로 응답값이 노출되는경우 엔티티를 반환하면 안된다.
+     */
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+}
