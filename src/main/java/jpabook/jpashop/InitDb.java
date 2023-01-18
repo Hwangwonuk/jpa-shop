a6666d9c3631cf6155d52fa13411e7a1946161e1
Hwangwonuk <hok0173@naver.com>
2023-01-18 11:45:27 +0900

    # API 개발 고급 - 준비 1. API 개발 고급 소개 2. 조회용 샘플 데이터 입력

---
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,122 @@
+/*
+ * Created by Wonuk Hwang on 2023/01/18
+ * As part of Bigin
+ *
+ * Copyright (C) Bigin (https://bigin.io/main) - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by infra Team <wonuk_hwang@bigin.io>, 2023/01/18
+ */
+package jpabook.jpashop;
+
+/**
+ * create on 2023/01/18. create by IntelliJ IDEA.
+ *
+ * <p> </p>
+ * <p> {@link } and {@link } </p> *
+ *
+ * @author wonukHwang
+ * @version 1.0
+ * @see
+ * @since (ex : 5 + 5)
+ */
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 종 주문 2개
+ * * userA
+ * 	 * JPA1 BOOK
+ * 	 * JPA2 BOOK
+ * * userB
+ * 	 * SPRING1 BOOK
+ * 	 * SPRING2 BOOK
+ */
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+  private final InitService initService;
+
+  @PostConstruct
+  public void init() {
+    initService.dbInit1();
+    initService.dbInit2();
+  }
+
+  @Component
+  @Transactional
+  @RequiredArgsConstructor
+  static class InitService {
+
+    private final EntityManager em;
+
+    public void dbInit1() {
+      System.out.println("Init1" + this.getClass());
+      Member member = createMember("userA", "서울", "1", "1111");
+      em.persist(member);
+
+      Book book1 = createBook("JPA1 BOOK", 10000, 100);
+      em.persist(book1);
+
+      Book book2 = createBook("JPA2 BOOK", 20000, 100);
+      em.persist(book2);
+
+      OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+      OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+      Delivery delivery = createDelivery(member);
+      Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+      em.persist(order);
+    }
+
+    public void dbInit2() {
+      Member member = createMember("userB", "진주", "2", "2222");
+      em.persist(member);
+
+      Book book1 = createBook("SPRING1 BOOK", 20000, 200);
+      em.persist(book1);
+
+      Book book2 = createBook("SPRING2 BOOK", 40000, 300);
+      em.persist(book2);
+
+      OrderItem orderItem1 = OrderItem.createOrderItem(book1, 20000, 3);
+      OrderItem orderItem2 = OrderItem.createOrderItem(book2, 40000, 4);
+
+      Delivery delivery = createDelivery(member);
+      Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+      em.persist(order);
+    }
+
+    private Member createMember(String name, String city, String street, String zipcode) {
+      Member member = new Member();
+      member.setName(name);
+      member.setAddress(new Address(city, street, zipcode));
+      return member;
+    }
+
+    private Book createBook(String name, int price, int stockQuantity) {
+      Book book1 = new Book();
+      book1.setName(name);
+      book1.setPrice(price);
+      book1.setStockQuantity(stockQuantity);
+      return book1;
+    }
+
+    private Delivery createDelivery(Member member) {
+      Delivery delivery = new Delivery();
+      delivery.setAddress(member.getAddress());
+      return delivery;
+    }
+  }
+}
